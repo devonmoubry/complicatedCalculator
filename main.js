d3ecc375d266faa5593b7f55484934ed84b8902e
Devon Moubry <devon@moubry.com>
2017-03-11 17:47:44 -0600

    display target.innerHTML to output.textContent addEventListener

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 var equals = document.getElementById('equals');
 var clearall = document.querySelector('div #clearAll');
 var buttons = document.querySelectorAll('div button');
+var calcOutput = document.querySelector('#output-area');
 
-buttons.addEventListener("click", function( event ) {
+
   // display the current click count inside the clicked div
 //  event.target.innerHTML = "click count: " + event.detail;
 //}, false);
 
 //.textContent
 //.outerText
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function pushingKeys(event) {
+  calcOutput.textContent = event.target.innerHTML;
+  });
+}
