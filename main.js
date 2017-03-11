6ed68526aef9e2143b871db9afe73e45b1f0a287
Devon Moubry <devon@moubry.com>
2017-03-11 16:12:47 -0600

    styles updated

---
+++ main.js
@@ -0,0 +1,11 @@
+var equals = document.getElementById('equals');
+var clearall = document.querySelector('div #clearAll');
+var buttons = document.querySelectorAll('div button');
+
+buttons.addEventListener("click", function( event ) {
+  // display the current click count inside the clicked div
+//  event.target.innerHTML = "click count: " + event.detail;
+//}, false);
+
+//.textContent
+//.outerText
