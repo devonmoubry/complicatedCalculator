21b8887faf960a2af25b4c527977d3e56cf593ae
Devon Moubry <devon@moubry.com>
2017-03-11 22:02:48 -0600

    numbers, operators, and AC work

--- main.js
+++ main.js
@@ -2,16 +2,96 @@ var equals = document.getElementById('equals');
 var clearall = document.querySelector('div #clearAll');
 var buttons = document.querySelectorAll('div button');
 var calcOutput = document.querySelector('#output-area');
+var displayScreen = calcOutput.textContent;
 
+clickCount = 0;
 
-  // display the current click count inside the clicked div
-//  event.target.innerHTML = "click count: " + event.detail;
-//}, false);
-
-//.textContent
-//.outerText
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function pushingKeys(event) {
-  calcOutput.textContent = event.target.innerHTML;
-  });
+
+    if (buttons[i] === clearall) {
+
+        clearall.addEventListener("click", function clearTheDecks() {
+            calcOutput.textContent = '';
+            clickCount = 0;
+
+        })
+
+    } else if (buttons[i] === equals) {
+
+        equals.addEventListener("click", function calcEquals(event) {
+
+            if (calcOutput.textContent.indexOf("+") !== -1) {
+
+                var opIndex = calcOutput.textContent.indexOf("+");
+                var a = calcOutput.textContent.substring(0, opIndex);
+                var b = calcOutput.textContent.substring(opIndex + 1);
+                calcOutput.textContent = parseInt(a) + parseInt(b);
+
+            } else if (calcOutput.textContent.indexOf("-") !== -1) {
+
+                var opIndex = calcOutput.textContent.indexOf("-");
+                var a = calcOutput.textContent.substring(0, opIndex);
+                var b = calcOutput.textContent.substring(opIndex + 1);
+                calcOutput.textContent = parseInt(a) - parseInt(b);
+
+            } else if (calcOutput.textContent.indexOf("*") !== -1) {
+
+                var opIndex = calcOutput.textContent.indexOf("*");
+                var a = calcOutput.textContent.substring(0, opIndex);
+                var b = calcOutput.textContent.substring(opIndex + 1);
+                calcOutput.textContent = parseInt(a) * parseInt(b);
+
+            } else if (calcOutput.textContent.indexOf("/") !== -1) {
+
+                var opIndex = calcOutput.textContent.indexOf("/");
+                var a = calcOutput.textContent.substring(0, opIndex);
+                var b = calcOutput.textContent.substring(opIndex + 1);
+                calcOutput.textContent = parseInt(a) / parseInt(b);
+
+            } else {
+
+                alert('Error! This is awkward. Please try again.');
+
+            }
+
+        });
+
+    } else {
+
+        buttons[i].addEventListener("click", function pushingKeys(event) {
+
+            calcOutput.textContent += event.target.innerHTML;
+
+            clickCount++;
+
+            if (calcOutput.textContent.length > 8 || clickCount > 8) {
+
+                alert('You are mathing too hard for me. Please go to google.');
+            }
+
+        });
+
+    }
+
 }
+
+//function findIndexOfOperator( displayScreen ) {
+
+//}
+
+
+//var a substring(leftside of operator)
+//var b substring(leftside of operators)
+
+//when =
+//get calcOutput.textContent
+//parseInt?
+//do it
+//calcOutput.textContent = it
+
+//indexOf "+"etc
+//substring ""
+//parseInt ""
+//return a "" b
+
+//addEventListener type: keydown to hear keys pressed to update
